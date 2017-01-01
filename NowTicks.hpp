9ed9433236b242735fb2b8289224b2a5c513fc3f
colinrg <colingirling@googlemail.com>
2017-01-01 23:26:44 +0000

    Upgraded NowTicks to provide the ability to track start and stop time and handle elapsed time.

--- NowTicks.hpp
+++ NowTicks.hpp
@@ -18,6 +18,7 @@ limitations under the License.
 #define OCL_GUARD_DATETIME_NOWTICKS_HPP
 
 #include <ctime>
+#include <cstddef>
 
 namespace ocl
 {
@@ -33,11 +34,81 @@ public:
 
     static time_type const TICKS_PER_SECOND = CLOCKS_PER_SEC;
 
+// Static methods.
 public:
     static time_type Now()
     {
         return static_cast<time_type>(::clock());
     }
+
+// Constructors and destructor.
+public:
+    NowTicks() throw()
+        : m_start(0)
+        , m_stop(0)
+        , m_elapsed_ref(NULL)
+    {
+    }
+
+    NowTicks(bool record_start)
+        : m_start(record_start ? Now() : 0)
+        , m_stop(0)
+        , m_elapsed_ref(NULL)
+    {
+    }
+
+    // Provide a reference to an elapsed time that can be updated when
+    // the object is destroyed or goes out of scope.
+    NowTicks(time_type& elapsed)
+        : m_start(Now())
+        , m_stop(0)
+        , m_elapsed_ref(&elapsed)
+    {
+    }
+
+    ~NowTicks()
+    {
+        if (m_elapsed_ref != NULL)
+            *m_elapsed_ref = Now() - m_start;
+    }
+
+// Member functions.
+public:
+    void Clear() throw()
+    {
+        m_start = m_stop = static_cast<time_type>(0);
+    }
+
+    // Reset the start and stop time to now time.
+    void Reset()
+    {
+        m_start = Now();
+        m_stop = m_start;
+    }
+
+    // Get elapsed time, and update the stop time if refresh is true.
+    time_type GetElapsed(bool refresh = true)
+    {
+        if (refresh)
+            m_stop = Now();
+        return m_stop - m_start;
+    }
+
+    time_type GetStart() const throw()
+    {
+        return m_start;
+    }
+
+    time_type GetStop() const throw()
+    {
+        return m_stop;
+    }
+
+// Data (internal use only)
+private:
+    time_type  m_start;
+    time_type  m_stop;
+    time_type* m_elapsed_ref;
 };
 
 } // namespace ocl
