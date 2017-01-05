5a832248ae807a6c6ee12298397aa9a7187d3708
colinrg <colingirling@googlemail.com>
2017-01-05 23:09:50 +0000

    rename variables to represent time and date.

--- NowDateTime.hpp
+++ NowDateTime.hpp
@@ -35,38 +35,38 @@ public:
 
 // Static methods.
 public:
-    static void ToDateTime(DateTime& date_time, tm const& tm_time) throw()
+    static void ToDateTime(DateTime& date_time, tm const& tm_dt) throw()
     {
         // Take a copy as this struct us shared across all threads.
-        tm local_tm_time = tm_time;
+        tm local_tm_dt = tm_dt;
 
-        date_time.SetHours(static_cast<DateTime::hour_type>(local_tm_time.tm_hour));
-        date_time.SetMinutes(static_cast<DateTime::minute_type>(local_tm_time.tm_min));
-        date_time.SetSeconds(static_cast<DateTime::second_type>(local_tm_time.tm_sec));
+        date_time.SetHours(static_cast<DateTime::hour_type>(local_tm_dt.tm_hour));
+        date_time.SetMinutes(static_cast<DateTime::minute_type>(local_tm_dt.tm_min));
+        date_time.SetSeconds(static_cast<DateTime::second_type>(local_tm_dt.tm_sec));
         date_time.SetMilliseconds(0U);
-        date_time.SetDay(static_cast<DateTime::day_type>(local_tm_time.tm_mday));
-        date_time.SetMonth(static_cast<DateTime::month_type>(local_tm_time.tm_mon) + DateTime::MIN_MONTH);
-        date_time.SetYear(static_cast<DateTime::year_type>(local_tm_time.tm_year + 1900));
+        date_time.SetDay(static_cast<DateTime::day_type>(local_tm_dt.tm_mday));
+        date_time.SetMonth(static_cast<DateTime::month_type>(local_tm_dt.tm_mon) + DateTime::MIN_MONTH);
+        date_time.SetYear(static_cast<DateTime::year_type>(local_tm_dt.tm_year + 1900));
     }
 
     static time_type Now(TimeZone time_zone = TimeZone::GMT)
     {
-        tm* tm_time = NULL;
+        tm* tm_dt = NULL;
         time_type date_time;
         time_t t = time(NULL);
 
         switch (time_zone)
         {
         case TimeZone::Local:
-            tm_time = localtime(&t);
+            tm_dt = localtime(&t);
             break;
         case TimeZone::GMT:
-            tm_time = gmtime(&t);
+            tm_dt = gmtime(&t);
             break;
         }
 
-        if (tm_time != NULL)
-            ToDateTime(date_time, *tm_time);
+        if (tm_dt != NULL)
+            ToDateTime(date_time, *tm_dt);
         return date_time;
     }
 };
